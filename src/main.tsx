7941878edd87c7bb5e7129d705dbe07127e69e38
Jack Lindgren <jackmarkcharleslindgren@gmail.com>
2026-06-09 11:04:05 -0600

    config tweaks

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { BrowserRouter } from "react-router";
 import { ThemeProvider } from "./components/theme-provider";
+import "./index.css";
 
 import App from "./App";
 
